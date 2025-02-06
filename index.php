a41da17571a88d84311e035b52ab68d747ad8f4f
ImJoselu <jltortolacer@gmail.com>
2025-02-06 13:48:16 +0100

    Bootstrap añadido

--- index.php
+++ index.php
@@ -12,26 +12,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ToDoList</title>
     <link rel="icon" href="/images/icon.png" type="image/x-icon" />
+    <link rel="stylesheet" href="styles.css" type="text/css" />
+
+    <!-- Archivo CSS Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap JS desde CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
-<h2>Lista de Tareas</h2>
+    <main>
+        <h2>Lista de Tareas</h2>
 
-<form action="procesar.php" method="post">
-    <input type="text" name="tarea" placeholder="Tarea Test" required>
-    <button type="submit"> Agregar </button>
-</form>
+        <form action="procesar.php" method="post">
+            <input type="text" name="tarea" placeholder="Tarea Test" required>
+            <button class="btn btn-primary" type="submit"> Agregar </button>
+        </form>
 
-<ul>
-    <?php if (!empty($tareas)): ?>
-        <?php foreach ($tareas as $index => $tarea): ?>
-            <li>
-                <?php echo htmlspecialchars($tarea); ?>
-                <a href="eliminar.php?index=<?php echo $index; ?>">❌</a>
-            </li>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <p>No hay tareas pendientes</p>
-    <?php endif; ?>
-</ul>
+        <ul>
+            <?php if (!empty($tareas)): ?>
+                <?php foreach ($tareas as $index => $tarea): ?>
+                    <li>
+                        <?php echo htmlspecialchars($tarea); ?>
+                        <a href="eliminar.php?index=<?php echo $index; ?>">❌</a>
+                    </li>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>No hay tareas pendientes</p>
+            <?php endif; ?>
+        </ul>
+    </main>
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+            <span class="text-muted">Mi Proyecto © <script>document.write(new Date().getFullYear())</script> </span>
+        </div>
+    </footer>
 </body>
 </html>
